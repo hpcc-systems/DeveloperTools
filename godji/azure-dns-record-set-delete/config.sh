5c79b6f0cc28f00561f870a0bb5591ec0d66407c
Godji Fortil <Godji.Fortil@LexisNexisRisk.Com>
2023-07-13 12:50:58 -0400

    Add a script to delete DNS record sets from Azure.
    Signed-off-by: Godji Fortil <Godji.Fortil@LexisNexisRisk.Com>

---
+++ godji/azure-dns-record-set-delete/config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Get the subscription name
+SUBSCRIPTION_NAME=""
+
+# Get the resource group name and zone name from the user.
+RESOURCE_GROUP_NAME=""
+DNS_ZONE_NAME=""
+
+# Get the keyword to select the dns record sets.
+# This should be common string in all the record names you want to delete.
+# Examples: hpcc, hpcc2, play
+KEYWORD=""
+
+# Types of DNS record sets
+RECORD_TYPES=("A" "TXT")
